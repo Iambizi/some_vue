65c3dbef11a9003954f70194d105e7d5c1686101
4wbizi <amir@ex-situ.com>
2020-02-27 22:05:18 -0500

    Commiting all files from create command

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Hero />
+        <Skills />
+        <Projects />
+    </div>
+</template>
+
+<script>
+import Hero from './components/Home';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+    export default {
+        components: {
+            Hero,
+            Skills,
+            Projects
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Project></Project>
+    </div>
+</template>
+
+<script>
+import Project from './components/Project';
+
+    export default {
+        components: {
+            Project,
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
