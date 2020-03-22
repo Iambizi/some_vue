c607742cb3fdcd37a9eb33d9f2854f1e14bf36b7
4wbizi <amir@ex-situ.com>
2020-03-22 13:52:19 -0400

    Fixes skills css

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -50,104 +50,130 @@
 <style lang="scss" scoped>
 #services-section {
   padding: 80px 120px;
-  .title {
-        position: relative;
-        width: 200px;
-        height: 200px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: 0 auto;
-        .circle {
-            background: #eef9ff;
-            width: 150px;
-            height: 150px;
-            border-radius: 50%;
-            position: absolute;
-            top: 23px;    
-        }
-    h1{
-        position: relative;
-        font-size: 2.5rem;
-        text-transform: uppercase;
-        color: #16171b;
-        font-weight: 100;
-        letter-spacing: 4px;  
-    }     
+}
+
+#services-section .title {
+  position: relative;
+  width: 200px;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+}
+
+#services-section .title .circle {
+  background: #eef9ff;
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  position: absolute;
+  top: 23px;
+}
+#services-section .title h1 {
+  position: relative;
+  font-size: 2.5rem;
+  text-transform: uppercase;
+  color: #16171b;
+  font-weight: 100;
+  letter-spacing: 4px;
+}
+
+#services-section .services-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 40px;
+}
+
+#services-section .services-container .box {
+  background: rgba(255, 255, 255, 0);
+  transition: all 0.4s ease-in-out;
+  padding: 40px;
+  cursor: pointer;
+}
+
+#services-section .services-container .box:hover {
+  background: rgba(255, 255, 255, 1);
+  -webkit-box-shadow: 10px 40px 74px 24px rgba(0, 0, 0, 0.08);
+  -moz-box-shadow: 10px 40px 74px 24px rgba(0, 0, 0, 0.08);
+  box-shadow: 10px 40px 74px 24px rgba(0, 0, 0, 0.08);
+}
+
+#services-section .services-container .box .fas {
+  font-size: 2rem;
+  margin-bottom: 2rem;
+}
+
+#services-section .services-container .box h5 {
+  font-size: 0.9rem;
+  font-weight: 700;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  margin-bottom: 2rem;
+}
+
+#services-section .services-container .box ul,
+p {
+  font-size: 0.8rem;
+  line-height: 1.5;
+  color: #b3b3b3;
+}
+
+#services-section .services-container .box.blue {
+  border-bottom: 2px solid #52b8ff;
+}
+
+#services-section .services-container .box.red {
+  border-bottom: 2px solid #ff5253;
+}
+
+#services-section .services-container .box.yellow {
+  border-bottom: 2px solid #ffc853;
+}
+
+/*Tablets */
+@media only screen and (max-width: 960px) {
+  #services-section .services-container {
+    grid-template-columns: 100%;
   }
-  .services-container{
-      display: grid;
-      grid-template-columns: 1fr 1fr 1fr;
-      grid-gap: 40px;
-      .box{
-        background: rgba(255, 255, 255, 0);
-        transition: all 0.4s ease-in-out;
-        padding: 40px;
-        cursor: pointer;
-        :hover{
-            background: rgba(255, 255, 255, 1);
-            -webkit-box-shadow: 10px 40px 74px 24px rgba(0, 0, 0, 0.08);
-            -moz-box-shadow: 10px 40px 74px 24px rgba(0, 0, 0, 0.08);
-            box-shadow: 10px 40px 74px 24px rgba(0, 0, 0, 0.08);    
-        }
-        .fas{
-           font-size: 2rem;
-           margin-bottom: 2rem; 
-        }
-        h5{
-            font-size: 0.9rem;
-            font-weight: 700;
-            text-transform: uppercase;
-            letter-spacing: 2px;
-            margin-bottom: 2rem;
-        }
-        ul,p{
-            font-size: .8rem;
-            line-height: 1.5;
-            color: #b3b3b3;
-        }
-        &.blue{
-            border-bottom: 2px solid #52b8ff;
-        }
-        &.red{
-            border-bottom: 2px solid #ff5253;
-        }
-        &.yellow{
-            border-bottom: 2px solid #ffc853;
-        }    
-    }
+  #services-section .services-container .box:hover {
+    background: rgba(255, 255, 255, 1);
+    -webkit-box-shadow: 0px 40px 74px 24px rgba(0, 0, 0, 0.08);
+    -moz-box-shadow: 0px 40px 74px 24px rgba(0, 0, 0, 0.08);
+    box-shadow: 0px 40px 74px 24px rgba(0, 0, 0, 0.08);
   }
 }
+
+@media only screen and (max-width: 767px) {
+}
+/*Phone*/
 @media only screen and (max-width: 450px) {
+  #services-section .title .circle {
+    width: 100px;
+    height: 100px;
+    top: 53px;
+    left: 45px;
+  }
   #services-section {
     padding: 35px 45px;
-    .title{
-        .circle {
-            width: 100px;
-            height: 100px;
-            top: 53px;
-            left: 45px;
-        }
-        h1{
-            right: 4px;
-            font-size: 1rem;
-            font-weight: 100;    
-        }
-    }
-    .services-container{
-        .box{
-            .fas{
-                font-size: 1.5rem;
-                margin-bottom: 2rem;
-            }
-            h5{
-                font-size: 0.7rem;
-            }
-            ul,p{
-                font-size: 0.6rem;
-            }
-        }
-    }
+  }
+  #services-section .title h1 {
+    right: 4px;
+    font-size: 1rem;
+    font-weight: 100;
+  }
+  #services-section .services-container .box .fas {
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+
+  #services-section .services-container .box h5 {
+    font-size: 0.7rem;
+  }
+
+  #services-section .services-container .box ul,
+  p {
+    font-size: 0.6rem;
   }
 }
 </style>
